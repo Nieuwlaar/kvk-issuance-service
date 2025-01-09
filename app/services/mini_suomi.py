97b0a1699ce002842a897fef55f5bf32d74d7d02
Nieuwlaar <nieuwlaar@gmail.com>
2025-01-09 17:07:48 +0100

    Test

--- app/services/mini_suomi.py
+++ app/services/mini_suomi.py
@@ -35,19 +35,19 @@ def issue_credential(credentialConfiguration: str, kvkNumber: str):
         one_year_from_now = now + timedelta(days=365)
         
         if credentialConfiguration == "LPIDSdJwt":
-            lpid_data = KVKBevoegdhedenAPI.get_lpid(kvkNumber)
-            payload = {
-                "legal_person_id": lpid_data["data"]["id"],
-                "legal_person_name": lpid_data["data"]["legal_person_name"],
-                "issuer_id": lpid_data["metadata"]["issuer_id"],
-                "issuer_name": lpid_data["metadata"]["issuing_authority_name"],
-                "issuer_country": lpid_data["metadata"]["issuing_country"],
-                "issuance_date": now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                "expire_date": one_year_from_now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                "credential_status": "active",
-                "authentic_source_id": lpid_data["metadata"]["issuer_id"],
-                "authentic_source_name": "Kamer van Koophandel"
-            }
+            # lpid_data = KVKBevoegdhedenAPI.get_lpid(kvkNumber)
+            # payload = {
+            #     "legal_person_id": lpid_data["data"]["id"],
+            #     "legal_person_name": lpid_data["data"]["legal_person_name"],
+            #     "issuer_id": lpid_data["metadata"]["issuer_id"],
+            #     "issuer_name": lpid_data["metadata"]["issuing_authority_name"],
+            #     "issuer_country": lpid_data["metadata"]["issuing_country"],
+            #     "issuance_date": now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            #     "expire_date": one_year_from_now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            #     "credential_status": "active",
+            #     "authentic_source_id": lpid_data["metadata"]["issuer_id"],
+            #     "authentic_source_name": "Kamer van Koophandel"
+            # }
             offer_id = "mockOfferId123"  # Generate a unique ID if necessary
             credential_offer_url = f"{ISSUER_DOMAIN}/mini-suomi/credential_offer?id={offer_id}"
 
@@ -58,52 +58,52 @@ def issue_credential(credentialConfiguration: str, kvkNumber: str):
             return {"credential_offer_uri": credential_offer_uri}
         
         elif credentialConfiguration == "EUCCSdJwt":
-            company_data = KVKBevoegdhedenAPI.get_company_certificate(kvkNumber)
-            legal_representatives = [
-                {
-                    "role": "J",
-                    "legalEntityId": company_data["data"]["registration_number"],
-                    "scopeOfRepresentation": "Jointly",
-                    "family_name": person["full_name"].split()[-1],
-                    "given_name": " ".join(person["full_name"].split()[:-1]),
-                    "birth_date": datetime.strptime(person["date_of_birth"], "%d-%m-%Y").strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-                }
-                for person in company_data["data"]["authorized_persons"]
-            ]
-            address_parts = company_data["data"]["postal_address"].split()
-            postal_code = next((part for part in address_parts if len(part) == 6 and part[:4].isdigit()), "")
+            # company_data = KVKBevoegdhedenAPI.get_company_certificate(kvkNumber)
+            # legal_representatives = [
+            #     {
+            #         "role": "J",
+            #         "legalEntityId": company_data["data"]["registration_number"],
+            #         "scopeOfRepresentation": "Jointly",
+            #         "family_name": person["full_name"].split()[-1],
+            #         "given_name": " ".join(person["full_name"].split()[:-1]),
+            #         "birth_date": datetime.strptime(person["date_of_birth"], "%d-%m-%Y").strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            #     }
+            #     for person in company_data["data"]["authorized_persons"]
+            # ]
+            # address_parts = company_data["data"]["postal_address"].split()
+            # postal_code = next((part for part in address_parts if len(part) == 6 and part[:4].isdigit()), "")
             
-            payload = {
-                "legalName": company_data["data"]["legal_person_name"],
-                "legalFormType": company_data["data"]["legal_form"],
-                "legalIdentifier": company_data["data"]["id"],
-                "registeredAddress": {
-                    "post_code": postal_code,
-                    "post_name": address_parts[-1] if address_parts else "",
-                    "thoroughfare": " ".join(address_parts[:-1]) if address_parts else ""
-                },
-                "postalAddress": {
-                    "post_code": postal_code,
-                    "post_name": address_parts[-1] if address_parts else "",
-                    "thoroughfare": " ".join(address_parts[:-1]) if address_parts else ""
-                },
-                "registrationDate": datetime.strptime(company_data["data"]["date_of_registration"], "%d-%m-%Y").strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                "legalEntityStatus": "active",
-                "legalRepresentative": legal_representatives,
-                "legalEntityActivity": [{"code": "", "businessDescription": ""}],
-                "contactPoint": {
-                    "contactPage": "",
-                    "hasEmail": company_data["data"]["electronic_address"],
-                    "hasTelephone": ""
-                },
-                "issuer_id": company_data["metadata"]["issuer_id"],
-                "issuer_name": company_data["metadata"]["issuing_authority_name"],
-                "issuer_country": company_data["metadata"]["issuing_country"],
-                "issuance_date": now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                "expire_date": one_year_from_now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
-                "authentic_source_id": company_data["metadata"]["issuer_id"],
-                "authentic_source_name": "Kamer van Koophandel"
-            }
+            # payload = {
+            #     "legalName": company_data["data"]["legal_person_name"],
+            #     "legalFormType": company_data["data"]["legal_form"],
+            #     "legalIdentifier": company_data["data"]["id"],
+            #     "registeredAddress": {
+            #         "post_code": postal_code,
+            #         "post_name": address_parts[-1] if address_parts else "",
+            #         "thoroughfare": " ".join(address_parts[:-1]) if address_parts else ""
+            #     },
+            #     "postalAddress": {
+            #         "post_code": postal_code,
+            #         "post_name": address_parts[-1] if address_parts else "",
+            #         "thoroughfare": " ".join(address_parts[:-1]) if address_parts else ""
+            #     },
+            #     "registrationDate": datetime.strptime(company_data["data"]["date_of_registration"], "%d-%m-%Y").strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            #     "legalEntityStatus": "active",
+            #     "legalRepresentative": legal_representatives,
+            #     "legalEntityActivity": [{"code": "", "businessDescription": ""}],
+            #     "contactPoint": {
+            #         "contactPage": "",
+            #         "hasEmail": company_data["data"]["electronic_address"],
+            #         "hasTelephone": ""
+            #     },
+            #     "issuer_id": company_data["metadata"]["issuer_id"],
+            #     "issuer_name": company_data["metadata"]["issuing_authority_name"],
+            #     "issuer_country": company_data["metadata"]["issuing_country"],
+            #     "issuance_date": now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            #     "expire_date": one_year_from_now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            #     "authentic_source_id": company_data["metadata"]["issuer_id"],
+            #     "authentic_source_name": "Kamer van Koophandel"
+            # }
                     # Construct the credential offer URL (this can point to an internal or mock endpoint)
             offer_id = "mockOfferId124"  # Generate a unique ID if necessary
             credential_offer_url = f"{ISSUER_DOMAIN}/mini-suomi/credential_offer?id={offer_id}"
