6329e94aa4c10812831551eac3878d1168049649
Nieuwlaar <nieuwlaar@gmail.com>
2025-01-09 14:52:41 +0100

    Update credential

--- app/services/mini_suomi.py
+++ app/services/mini_suomi.py
@@ -133,11 +133,11 @@ def generate_credential_jwt(credential_type: str, kvk_number: str) -> str:
                 "issuer_id": lpid_data["metadata"]["issuer_id"],
                 "issuer_name": lpid_data["metadata"]["issuing_authority_name"],
                 "issuer_country": lpid_data["metadata"]["issuing_country"],
-                "issuance_date": None,
-                "expire_date": None,
+                "issuance_date": now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                "expire_date": one_year_from_now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                 "credential_status": "active",
                 "authentic_source_id": lpid_data["metadata"]["issuer_id"],
-                "authentic_source_name": lpid_data["metadata"]["issuing_authority_name"]
+                "authentic_source_name": "Kamer van Koophandel"
             }
 
             # Generate salted disclosures for each claim
