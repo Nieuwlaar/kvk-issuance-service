85753d48b88158349e92f4c168d077388185b193
Nieuwlaar <nieuwlaar@gmail.com>
2025-01-09 17:23:43 +0100

    Dirty fix redone in EUCCSdJwt

--- app/services/mini_suomi.py
+++ app/services/mini_suomi.py
@@ -317,7 +317,8 @@ def generate_credential_jwt(credential_type: str, kvk_number: str) -> str:
                 "issuance_date": now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                 "expire_date": one_year_from_now.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
                 "authentic_source_id": company_data["metadata"]["issuer_id"],
-                "authentic_source_name": "Kamer van Koophandel"
+                "authentic_source_name": "Kamer van Koophandel",
+                "authentic_source_name2": "Kamer van Koophandel",
             }
 
             # Generate salted disclosures for each claim
