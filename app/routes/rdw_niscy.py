b4ac285246793de8ee8d83aa82e236658bf873cf
Nieuwlaar <nieuwlaar@gmail.com>
2025-03-27 16:56:41 +0100

    bug fix

--- app/routes/rdw_niscy.py
+++ app/routes/rdw_niscy.py
@@ -26,7 +26,7 @@ async def create_power_of_representation(request: PowerOfRepresentationRequest):
         # Initialize Chrome in headless mode with optimized settings
         options = webdriver.ChromeOptions()
         options.binary_location = "/snap/bin/chromium"
-        options.add_argument('--headless')
+        options.add_argument('--headless=new')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-gpu')
